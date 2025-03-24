3f4b4023f6593de340bf7fda2b778f0a74a88e25
Harjeet Singh <harjeet.chhabada@josys.com>
2025-03-24 12:36:04 +0530

    Initial commit

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { defineSchema, defineFunction } from '@aws-amplify/backend';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -12,12 +13,26 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+    Release: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+});
+
+
+export const sayHello = defineFunction({
+  name: 'say-hello',
+  entry: '../functions/say-hello/handler.ts'
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
+  functions: {
+    sayHello
+  },
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
